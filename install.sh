58d6a11094a68f68ab6e0e13973c859496a7bf57
anatolii2makaroff <anatolii.makaroff@gmail.com>
2018-10-27 23:43:23 +0300

    add erlang installer

---
+++ install.sh
@@ -0,0 +1,16 @@
+#
+# Install erlang from official github repo
+#
+
+yum install -y ncurses-devel
+
+git clone https://github.com/erlang/otp.git
+cd ./otp
+git checkout maint-20
+
+./otp_build autoconf
+./configure --prefix=/opt/erlang
+
+make 
+make install
+
