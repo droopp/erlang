1dad0c7c847abdcd41666e7c6aeeba77e1b54768
anatolii2makaroff <anatolii.makaroff@gmail.com>
2022-06-03 05:12:02 +0000

    add deps

--- install.sh
+++ install.sh
@@ -4,11 +4,17 @@
 
 set -e
 
+is_u="$(awk -F= '/^NAME/{print $2}' /etc/os-release | grep Ubuntu | wc -l)"
+if [[ "$is_u" -eq "0" ]]; then
+    yum install -y ncurses-devel
+else
+    apt-get install libncurses5-dev libncursesw5-dev
+fi
+
 git clone https://github.com/erlang/otp.git
 cd ./otp
 git checkout maint-25
 
-./otp_build autoconf
 ./configure --prefix=/opt/erlang
 
 make 
