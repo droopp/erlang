d7d903e547cbb7c47584f9d3153a00867771782c
anatolii2makaroff <anatolii.makaroff@gmail.com>
2022-06-03 18:07:06 +0000

    add comments

--- install.sh
+++ install.sh
@@ -4,12 +4,16 @@
 
 set -e
 
+# Install deps
+
 is_u="$(awk -F= '/^NAME/{print $2}' /etc/os-release | grep Ubuntu | wc -l)"
-if [[ "$is_u" -eq "0" ]]; then
-    yum install -y ncurses-devel
-else
+if [[ "$is_u" -eq "1" ]]; then
     apt-get install -y libncurses5-dev libncursesw5-dev
+else
+    yum install -y ncurses-devel
 fi
+
+# Build from source
 
 git clone https://github.com/erlang/otp.git
 cd ./otp
